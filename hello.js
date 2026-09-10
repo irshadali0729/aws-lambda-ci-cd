aa12d4a9a7c0b737ef00abb6f95ff636e078b0f2
codeaffairs <codeaffairs729@gmail.com>
2026-09-10 22:47:13 +0530

    testing cicd 2

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "Production grade CI CD" }),
+    body: JSON.stringify({ message: "Production grade CI CD2222" }),
   };
 };
