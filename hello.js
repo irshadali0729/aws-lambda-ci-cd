e1a05cf374627c67ee3234b9ff9daede143a9227
codeaffairs <codeaffairs729@gmail.com>
2026-09-10 22:43:41 +0530

    testing ci cd after changes

--- hello.js
+++ hello.js
@@ -2,6 +2,6 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ message: "Hello from YouTube demo API" }),
+    body: JSON.stringify({ message: "Production grade CI CD" }),
   };
 };
